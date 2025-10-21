b0946cc5025760330b400eb10b272a78c1931feb
Lokesh1-droid <lokeshpartha0604@gmail.com>
2025-10-21 19:00:00 +0530

    feat:added some cards in project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Propsin from './Propsin.jsx'
+import imagein from './assets/imagein.jpg'
+function App() {
+
+  return (
+    <>
+    <Propsin name='Card1' text="This is card of Card1" image={imagein}/>
+    <Propsin name='Card2' text="This is card of Card2" image={imagein}/>
+    <Propsin name="Card3" text="This is card of Card3" image={imagein}/>
+    </>
+  );
+}
+
+
+export default App
---
+++ src/Propsin.jsx
@@ -0,0 +1,13 @@
+function Propsin(props){
+  return(
+    <>
+      <div className = "card">
+      <img src = {props.image} alt="" />
+        <h3>{props.name}</h3>
+        <p>{props.text}</p>
+    </div>
+    </>
+    
+    );
+}
+export default Propsin
