2cdd5f0ca3c7f73d072d8cfd96432015fe7330b4
Lokesh1-droid <lokeshpartha0604@gmail.com>
2025-10-21 19:40:58 +0530

    try to change some details

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <>
     <Propsin name='Card1' text="This is card of Card1" image={imagein}/>
     <Propsin name='Card2' text="This is card of Card2" image={imagein}/>
-    <Propsin name="Card3" text="This is card of Card3" image={imagein}/>
+    <Propsin text="This is card of Card3" image={imagein}/>
     </>
   );
 }
--- src/Propsin.jsx
+++ src/Propsin.jsx
@@ -1,13 +1,11 @@
 function Propsin(props){
   return(
-    <>
       <div className = "card">
       <img src = {props.image} alt="" />
         <h3>{props.name}</h3>
         <p>{props.text}</p>
     </div>
-    </>
-    
     );
 }
+
 export default Propsin
